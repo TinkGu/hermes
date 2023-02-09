0348c1434be2b5c45b24017931a6eb1538ef97bc
TinkGu <tlwrite@126.com>
2023-02-09 21:06:31 +0800

    feat: 自动订阅 rss 更新执行自动化任务

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,2 +1,13 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
+
+export interface EnvValues {
+  /** json store 地址，记录了 rss 的更新信息 */
+  rssDbUrl: string;
+  /** raindrop API Token */
+  raindropToken: string;
+  /** 钉钉机器人 webhook 地址 */
+  webhook: string;
+  /** 钉钉机器人 secret */
+  secret: string;
+}
